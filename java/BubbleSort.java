be2b7997e8d51327ddc96a42bc369ddcfaa7e18f
Ahmed Elafifi <30698089+elafifi@users.noreply.github.com>
2018-10-30 22:29:25 +0200

    Update BubbleSort.java

--- java/BubbleSort.java
+++ java/BubbleSort.java
@@ -3,6 +3,9 @@ Author: Ahmed Elafifi
 Date: 30/10/2018
 category:Bubble Sort
 */
+
+//no. of comparisons = (n^2)/2
+//no. of swaps = (n^2)/4          (in worst case in reversed sorted array     no. of swaps = no. of comparisons = (n^2)/2)
 public void bubbleSort()
 {
     int out, in;
