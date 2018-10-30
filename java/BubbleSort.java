939a3876d466af333db1e9b824a00d8429834857
Ahmed Elafifi <30698089+elafifi@users.noreply.github.com>
2018-10-30 18:22:57 +0200

    Create BubbleSort.java

---
+++ java/BubbleSort.java
@@ -0,0 +1,12 @@
+Author: Ahmed Elafifi
+Date: 30/10/2018
+category:Bubble Sort
+
+public void bubbleSort()
+{
+    int out, in;
+    for(out=nElems-1; out>1; out--) // outer loop (backward)
+    for(in=0; in<out; in++) // inner loop (forward)
+    if( a[in] > a[in+1] ) // out of order?
+    swap(in, in+1); // swap them
+} // end bubbleSort()
