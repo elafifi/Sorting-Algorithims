be8e881ff1c71041cabd1b5ecc36aaf9a8451a3c
Ahmed Elafifi <30698089+elafifi@users.noreply.github.com>
2018-10-31 00:08:58 +0200

    Update InsertionSort.java

--- java/InsertionSort.java
+++ java/InsertionSort.java
@@ -1,11 +1,24 @@
+/**
+* Name:Ahmed Elafifi 
+* Date: 30/10/2018
+* category: Insertion Sort
+*/
+//no. of comparisons: n*(n-1)/4
+//no. of copies: n*(n-1)/4
+//for random data this algorithm runs twice as fast as the bubble sort and faster than the selection sort.
+
 public void insertionSort()
 {
-int in, out;
-for(out=1; out<nElems; out++) // out is dividing line
-{
-long temp = a[out]; // remove marked item
-in = out; // start shifts at out
-while(in>0 && a[in-1] >= temp) // until one is smaller,
-{
-a[in] = a[in-1]; // shift item right,
-Insertion Sort 99
+    int in, out;
+    for(out=1; out<nElems; out++) // out is dividing line
+    {
+    long temp = a[out]; // remove marked item
+    in = out; // start shifts at out
+    while(in>0 && a[in-1] >= temp) // until one is smaller,
+    {
+    a[in] = a[in-1]; // shift item right,
+    --in; // go left one position
+    }
+    a[in] = temp; // insert marked item
+    } // end for
+} //
